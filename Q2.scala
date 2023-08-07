22fbe282b930245f3d5f05c5017b3789969bb3ed
Dasun Thathsara <dasun.thathsara.sri@gmail.com>
2023-08-08 01:12:04 +0530

    Add files via upload

---
+++ Q2.scala
@@ -0,0 +1,10 @@
+package P7
+
+object Q2 extends App{
+  def calculateSquare(arr: Array[Int]): Array[Int] = {
+    arr.map(arr => arr * arr);
+  }
+
+  val numbers = calculateSquare(Array(1, 2, 3, 4, 5));
+  println(numbers.mkString(", "));
+}
